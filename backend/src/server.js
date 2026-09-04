5bc94f6b5e486f933a77a8cd6f5f803712c4ce97
arpankr0 <arpankr06@gmail.com>
2026-09-04 19:30:32 +0530

    update profile endpoint created

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,13 @@ import dotenv from 'dotenv';
 import authRoute from './routes/auth.route.js';
 import messageRoute from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 const app = express();
  
  const PORT = process.env.PORT || 3000;
   app.use(express.json());
+  app.use(cookieParser());
  app.use('/api/auth', authRoute);
  app.use('/api/messages', messageRoute);
 app.listen(PORT, () => {
